67a72f62dea2e0357360f2aea47d853cd6fc38de
Gregor Albert <gregoralbert@gmail.com>
2019-06-12 22:09:37 +0200

    test coordinates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'gyroscope';
+
+  public info = 'empty';
+
+  @HostListener('window:deviceorientation', ['$event'])
+  onClick(targetElement: any) {
+    this.info = targetElement.alpha + ' ' + targetElement.beta + ' ' + targetElement.gamma;
+  }
 }
+
+
